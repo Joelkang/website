802e87e18a983646e4b093c8a7b7bfb8d36b52c6
Joel Kang <joel@joelkang.com>
2025-03-20 22:59:52 +0000

    fix(posts-list): better styling for embed

--- app/pitstops/posts-list.tsx
+++ app/pitstops/posts-list.tsx
@@ -4,8 +4,9 @@ import type { listPosts } from "@/lib/content/atproto";
 import { formatDate } from "@/lib/utils/dates";
 import { AppBskyEmbedExternal } from "@atproto/api";
 import { Link } from "@/components/link";
-import { use } from "react";
+import React, { use } from "react";
 import { RichText } from "@atproto/api";
+import { ExternalLink } from "lucide-react";
 
 export function PostsList({
   listPromise,
@@ -74,19 +75,26 @@ export function PostsList({
                       </Link>
                     );
 
-                  return <span key={segment.text}>{segment.text}</span>;
+                  return segment.text.split("\n").map((textPart, idx, arr) => (
+                    <React.Fragment key={`${textPart}-${idx}`}>
+                      {textPart}
+                      {idx < arr.length - 1 && <br />}
+                    </React.Fragment>
+                  ));
                 })}
               </p>
 
               {externalEmbed && (
-                <Link href={externalEmbed.uri} target="_blank">
-                  <div className="flex flex-col rounded border border-neutral-400 p-2 gap-1 text-sm">
-                    <p>{externalEmbed.title}</p>
+                <Link href={externalEmbed.uri} className="group hover:text-white" target="_blank">
+                  <div className="flex flex-col rounded-lg border border-neutral-400 gap-1 text-sm">
+                    <p className="px-2 pt-2 font-semibold">{externalEmbed.title}</p>
                     {externalEmbed.description && (
                       <p>{externalEmbed.description}</p>
                     )}
-                    {externalEmbed.thumb && <p>{externalEmbed.description}</p>}
-                    <span>{new URL(externalEmbed.uri).hostname}</span>
+                    <span className="border-t bg-neutral-400/30 group-hover:text-brand border-neutral-400 pt-1 mt-1 pb-2 px-2 flex items-center gap-1">
+                      {new URL(externalEmbed.uri).hostname}{" "}
+                      <ExternalLink size={12} />
+                    </span>
                   </div>
                 </Link>
               )}
