82aa794d50e0aabc7f6d753ff79f14abbc92d068
Joel Kang <joel@joelkang.com>
2025-03-20 23:58:06 +0000

    fix(atproto): inline login function

--- app/lib/content/atproto.ts
+++ app/lib/content/atproto.ts
@@ -3,24 +3,16 @@ import { AtpAgent, AppBskyFeedPost } from "@atproto/api";
 const agent: AtpAgent = new AtpAgent({
   service: "https://bsky.social",
 });
-
-async function login() {
-  if (agent.hasSession && agent.session)
-    return (await agent.resumeSession(agent.session)).data;
-
+export async function listPosts(count?: number) {
   const me = await agent.login({
     identifier: "joel@joelkang.com",
     password: `${process.env.BLUESKY_PASSWORD}`,
   });
 
   if (!me.success) throw me.data;
-  return me.data;
-}
-
-export async function listPosts(count?: number) {
-  const me = await login();
+  
   const feedResp = await agent.app.bsky.feed.getAuthorFeed({
-    actor: me.did,
+    actor: me.data.did,
     filter: "posts_and_author_threads",
   });
   if (!feedResp.success) throw feedResp.data;
@@ -28,7 +20,7 @@ export async function listPosts(count?: number) {
   const posts = await Promise.all(
     feedResp.data.feed
       .filter((feedItem) => {
-        const isMine = feedItem.post.author.did === me.did;
+        const isMine = feedItem.post.author.did === me.data.did;
         return isMine && AppBskyFeedPost.isRecord(feedItem.post.record);
       })
       .map(async (feedPost) => {
